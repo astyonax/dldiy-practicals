48b896c100a82ca2ec4e0ede9c1252ef4aa2a57b
Guglielmo Saggiorato <astyonax@gmail.com>
2018-01-08 10:21:20 +0100

    in preparation to GEA for hyper-params

--- myexamples/learn_trafic/glue_validations.py
+++ myexamples/learn_trafic/glue_validations.py
@@ -3,16 +3,13 @@ import os
 import glob
 
 if __name__ == '__main__':
-    try:
-        os.makedirs('gifs')
-    except OSError:
-        pass
+    indir = sys.argv[1]
 
-    names = [j.split('/')[1] for j in glob.glob('3000_epochs/*')]
-    CMD = 'convert -loop 0 -delay 75 *_epochs/{name:s}/validation.png gifs/{name:s}.gif'
-    for name in names:
-
-        go = CMD.format(name=name)
-        print go
-        os.system(go)
-        
+    names = glob.glob(indir+'/*_epochs')
+    names = sorted(names,key=lambda x:int(x.split('/')[1].split('_')[0]))
+    # print names
+    files = ' '.join([j+'/validation.png' for j in names])
+    CMD = 'convert -loop 0 -delay 50 {infiles:s} {indir:s}/validation.gif'.format(infiles=files,indir=indir)
+    # print CMD
+    # exit()
+    os.system(CMD)
--- myexamples/learn_trafic/lstm.py
+++ myexamples/learn_trafic/lstm.py
@@ -5,13 +5,17 @@ from torch import nn
 import torch
 from sklearn.preprocessing import RobustScaler
 from tqdm import trange
-import warnings
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.ERROR)
 import os
 # load data file
-warnings.warn('fitting only on closing price')
-# time_series = np.load(open('../data/offline_prices.npy','r'))[:,:,-1]
 time_series = np.load(open('data/time_series_traffic_paris.npy','r'))
-
+h = np.arange(time_series.shape[0],dtype=np.float32)%24
+ch = np.cos(h/24.)
+sh = np.sin(h/24.)
+time_series = np.c_[h,ch,sh,time_series]
+print time_series.shape
+# exit()
 # scale data
 scaler = RobustScaler()
 Q = scaler.fit_transform(time_series)
@@ -21,6 +25,7 @@ WEIGHT_DECAY = 1e-5
 NESTEROV = True
 MOMENTUM = 0.9
 BATCH_SIZE = 600
+LR = 1/10.
 # Parameters
 FEATURES = Q.shape[-1]
 
@@ -31,6 +36,7 @@ def batch_gen(data,window_size,batch_size=BATCH_SIZE,shuffle=True):
     ws = window_size
     windows = [[data[i+n] for i in range(0, len(data)-1-ws, ws)] for n in range(ws)]
     windows = np.asarray(windows).transpose(1,0,-1)
+    # print windows.shape
     #~
     if shuffle:
         index = np.random.permutation(windows.shape[0])
@@ -48,6 +54,7 @@ def validate(Q,model,W):
     real = []
     pred = []
     model.eval()
+    W = W - 1
     for i in xrange(W,min(Q.shape[0]-1,200)): # min to limit time spent on this
         t0 = Q[i-W:i]
         t1 = Q[i]
@@ -64,7 +71,7 @@ def validate(Q,model,W):
     return MSEscore,{'real':real,'pred':pred}
 
 ## Training
-def new_train_model(batches_gen,data,model,window_size,epochs=1000,train=True,ev=1):
+def train_model(batches_gen,data,model,window_size,epochs=1000,train=True,ev=1):
     if train:
         model.train()
     else:
@@ -78,6 +85,7 @@ def new_train_model(batches_gen,data,model,window_size,epochs=1000,train=True,ev
         for x,y in batches:
             out = model(x)
             loss = model.loss(out,y)
+            # print out.size(),y.size(),x.size()
             model.zero_grad()
             loss.backward()
             model.optimizer.step()
@@ -92,30 +100,37 @@ def new_train_model(batches_gen,data,model,window_size,epochs=1000,train=True,ev
 
 # model(s)
 class RNN(nn.Module):
-    def __init__(self, inout_dim, hidden_dim =256, lr = 0.001):
+    def __init__(self, features, hidden_dim =256, lr = 0.001,weight_decay = WEIGHT_DECAY):
+        """
+            this class will have to contain also:
+            1. the scaler used for training (eg. RobustScaler)
+            2. the parameters used for trainig (eg window)
+            3. the input generation preparation (eg. np2var)
+        """
         super(RNN, self).__init__()
 
-        self._inout_dim = inout_dim
+        # self._inout_dim = features
         self._hidden_dim = hidden_dim
-        self.lin_in = nn.Linear(inout_dim,hidden_dim)
+        self.lin_in = nn.Linear(features,hidden_dim)
         self.cell = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim, batch_first=True)
-        self.lin_out = nn.Linear(hidden_dim,inout_dim)
+        self.lin_out = nn.Linear(hidden_dim,features)
 
         # functions
         self.__lr = lr
+        self.__weight_decay = weight_decay
         self.loss = nn.MSELoss()
         self.optimizer = self.__init_optimizer()
 #         self.activation = torch.nn.Activation('linear')
 
     def forward(self, c):
-        hidden = (Variable(torch.randn(1,1,self._hidden_dim)),
-                  Variable(torch.randn(1,1,self._hidden_dim)))
+        hidden = (Variable(torch.zeros(1,1,self._hidden_dim)),
+                  Variable(torch.zeros(1,1,self._hidden_dim)))
 
         output = self.lin_in(c)
         output,hidden = self.cell(output,hidden)
         output = self.lin_out(output)
-        output = output.max(dim=1)[0]
-#         output = self.activation(output)
+        # print output.size()
+        # output = output.max(dim=1)[0]
         return output
 
     @property
@@ -127,46 +142,56 @@ class RNN(nn.Module):
         self.__lr = new_lr
         self.optimizer = self.__init_optimizer()
 
+    @property
+    def weight_decay(self):
+        return self.__weight_decay
+
+    @weight_decay.setter
+    def weight_decay(self,new_wd):
+        self.__weight_decay=new_wd
+        self.optimizer = self.__init_optimizer()
+
     def __init_optimizer(self):
         return torch.optim.SGD(self.parameters(),
                                lr = self.__lr,
                                momentum=MOMENTUM,
-                               weight_decay=WEIGHT_DECAY,
+                               weight_decay=self.__weight_decay,
                                nesterov=NESTEROV)
 
 # main
-def main(W,HIDDEN_DIM,EPOCHS,EV=1000):
+def main(WINDOW,HIDDEN_DIM,EPOCHS,WEIGHT_DECAY,EV=1000):
     # os stuff
-    DIR = '{}_{}/'.format(W,HIDDEN_DIM)
+    DIR = '{}_{}_{}/'.format(WINDOW,HIDDEN_DIM,WEIGHT_DECAY)
     print DIR
 
     already_done_epochs = 0
     try:
         os.makedirs(DIR)
     except OSError:
-        # warnings.warn("overwriting previous files in folder "+DIR)
+        # logging.warninging("overwriting previous files in folder "+DIR)
         try:
             already_done_epochs = int(open(DIR+'/epoch').read())
-            warnings.warn('found {0:d} epochs done'.format(already_done_epochs))
+            logging.warning('found {0:d} epochs done'.format(already_done_epochs))
         except IOError:
             pass
     # load from last epoch
     oDIR = DIR + '/{0:d}_epochs/'.format(already_done_epochs)
     try:
         rnn = torch.load(oDIR+'model.pth')
-        warnings.warn('Continuing with previous training up to {EPOCHS:d} epochs'.format(EPOCHS=EPOCHS))
+        fitness = float(open(oDIR + 'fitness','r').read())
+        logging.warning('Continuing with previous training up to {EPOCHS:d} epochs'.format(EPOCHS=EPOCHS))
     except:
-        warnings.warn('Starting a new training sequence from scratch')
-        rnn = RNN(FEATURES,hidden_dim=HIDDEN_DIM)
+        logging.warning('Starting a new training sequence from scratch')
+        rnn = RNN(FEATURES,hidden_dim=HIDDEN_DIM,weight_decay=WEIGHT_DECAY)
         already_done_epochs = 0
 
-    rnn.lr=0.1
+    rnn.lr=LR
     for millennium in xrange(already_done_epochs,EPOCHS,EV):
         print millennium,'->',millennium + EV
         # train
-        train_history = new_train_model(batch_gen,Q,rnn,epochs=EV,ev=EV/2,window_size=W)
+        train_history = train_model(batch_gen,Q,rnn,epochs=EV,ev=EV/2,window_size=WINDOW)
         # validate
-        validation = validate(Q,rnn,W)[1]
+        fitness, validation = validate(Q,rnn,WINDOW)
         # package output
         oDIR = DIR + '/{0:d}_epochs/'.format(millennium+EV)
         try:
@@ -177,6 +202,7 @@ def main(W,HIDDEN_DIM,EPOCHS,EV=1000):
         torch.save(rnn,oDIR+'model.pth')
         # save validation
         np.savez(open(oDIR+'validation.npz','wb'),**validation)
+        os.system('python plot_validation.py {0:s}'.format(oDIR+'validation.npz'))
         # save log train_history
         with open(oDIR + 'train_history.txt','w') as fout:
             fout.write(train_history)
@@ -186,6 +212,11 @@ def main(W,HIDDEN_DIM,EPOCHS,EV=1000):
 
         with open(DIR+'epoch','w') as fout:
             fout.write(str(millennium+EV))
+
+        with open(oDIR + 'fitness','w') as fout:
+            fout.write(str(fitness))
+
+    return fitness
 
 if __name__ == '__main__':
     from fire import Fire
